209fbff0a4712127db86490f8fe921283156076d
unknown <leshhuk.2000@mail.ru>
2020-09-02 08:32:47 +0300

    add comments and fix mistakes

--- predict.py
+++ predict.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 class PredictTarget:
     def __init__(self, model, encoder):
         self.model = model
@@ -5,9 +8,10 @@ class PredictTarget:
         self.encoder = encoder
 
     def predict(self, X):
-        self.target = self.model.predict(X)
+        self.target = pd.Series(self.model.predict(X))
         self.inverse_transform_target()
+        self.target = pd.Series(self.target, index=X.index.values)
 
-    def inverse_transform_target(self):
+    def inverse_transform_target(self):  # transform from [0,0,1,...] to ['In-out','In-out','AM',...]
         self.target = self.target.replace({1: 0, 0: 1})
-        self.target = self.model.inverse_transform(self.target)
+        self.target = self.encoder.inverse_transform(self.target)
--- preprocess.py
+++ preprocess.py
@@ -8,101 +8,96 @@ class PreprocessData:
         self.create_new_columns()
         self.drop_unnec_col()
 
-    def create_new_columns(self):
-        self.df['New_SKU'] = self.df['Формат'] + '_' + self.df['SKU']
+    def create_new_columns(self):  # create price and Format_SKU = 'Формат' + 'SKU'
+        self.df['Format_SKU'] = self.df['Формат'] + '_' + self.df['SKU']
         self.df['цена'] = self.df['Сумма'] / self.df['Кол-во уп']
-        self.df['New_SKU'], self.df['цена'] = self.df['цена'], self.df['New_SKU']
-        self.df = self.df.rename(columns={'New_SKU': 'цена', 'цена': 'New_SKU'}, inplace=True)
+        self.df['Format_SKU'], self.df['цена'] = self.df['цена'], self.df['Format_SKU']
+        self.df = self.df.rename(columns={'Format_SKU': 'цена', 'цена': 'Format_SKU'})
 
-    def drop_unnec_col(self):
+    def drop_unnec_col(self):  # drop useless features
         unnecessary = ['Brand', 'Категория', 'Формат', 'SKU']
         self.df = self.df.drop(unnecessary, axis=1)
 
 
-def find_difference_right(value):
-    a = list(value)
-    while 0 in a:
-        a.remove(0)
-    a = a[::-1]
-    number = int(np.argmax(a))
-    min_value = a[0]
+def find_difference_right(value):  # find how many times max value more than min value after max value
+    value = list(value)
+    while 0 in value:
+        value.remove(0)
+    value = value[::-1]
+    number = int(np.argmax(value))
+    min_value = value[0]
     for i in range(number):
-        if min_value > a[i]:
-            min_value = a[i]
-    return a[number] / min_value
+        if min_value > value[i]:
+            min_value = value[i]
+    return value[number] / min_value
 
 
-def find_min_quantites(value):
-    a = list(value)
-    while 0 in a:
-        a.remove(0)
-    len_time = len(a)
+def find_min_quantites(value):  # find quantity of local min
+    value = list(value)
+    while 0 in value:
+        value.remove(0)
+    len_time = len(value)
     counter = 0
     for i in range(1, len_time - 1):
-        if a[i - 1] > a[i] and a[i] < a[i + 1]:
+        if value[i - 1] > value[i] and value[i] < value[i + 1]:
             counter += 1
-        else:
-            pass
     return counter
 
 
-def find_max_quantites(value):
-    a = list(value)
-    while 0 in a:
-        a.remove(0)
-    len_time = len(a)
+def find_max_quantites(value): # find quantity of local max
+    value = list(value)
+    while 0 in value:
+        value.remove(0)
+    len_time = len(value)
     counter = 0
     for i in range(1, len_time - 1):
-        if a[i - 1] < a[i] and a[i] > a[i + 1]:
+        if value[i - 1] < value[i] and value[i] > value[i + 1]:
             counter += 1
-        else:
-            pass
     return counter
 
 
-def find_difference_left(value):
-    a = list(value)
-    while 0 in a:
-        a.remove(0)
-    number = int(np.argmax(a))
-    min_value = a[0]
+def find_difference_left(value):  # find how many times max value more than min value before max value
+    value = list(value)
+    while 0 in value:
+        value.remove(0)
+    number = int(np.argmax(value))
+    min_value = value[0]
     for i in range(number):
-        if min_value > a[i]:
-            min_value = a[i]
-    return a[number] / min_value
+        if min_value > value[i]:
+            min_value = value[i]
+    return value[number] / min_value
 
 
-def find_min(value):
-    a = list(value)
-    while 0 in a:
-        a.remove(0)
-    len_time = len(a)
-    b = a[::-1]
-    min_value = b[0]
+def find_min(value):  # find quantity of global min
+    value = list(value)
+    while 0 in value:
+        value.remove(0)
+    len_time = len(value)
+    back_value = value[::-1]
+    min_value = back_value[0]
     counter = 0
     for i in range(len_time - 1):
-        if min_value > b[i + 1]:
+        if min_value > back_value[i + 1]:
             counter += 1
-            min_value = b[i + 1]
+            min_value = back_value[i + 1]
     return counter
 
 
-def find_max(value):
-    a = list(value)
-    while 0 in a:
-        a.remove(0)
-    len_time = len(a)
-    b = a[::-1]
-    max_value = a[0]
+def find_max(value):  # find quantity of global max
+    value = list(value)
+    while 0 in value:
+        value.remove(0)
+    len_time = len(value)
+    max_value = value[0]
     counter = 0
     for i in range(len_time - 1):
-        if max_value < a[i + 1]:
+        if max_value < value[i + 1]:
             counter += 1
-            max_value = a[i + 1]
+            max_value = value[i + 1]
     return counter
 
 
-def find_quantity_month(values):
+def find_quantity_month(values):  # find quantity of month where was selling something
     a = list(values)
     while 0 in a:
         a.remove(0)
@@ -116,18 +111,18 @@ class CreateNewData:
         self.create_data()
         self.fill_new_data()
 
-    def create_data(self):
+    def create_data(self):  # create new pandas DataFrame with new features
         df_columns = ['products', 'difference_products', 'all_price', 'difference_all_price',
                       'shops', 'difference_shops', 'price', 'difference_price']
         function_columns = ['global_max', 'global_min', 'difference_left', 'difference_right', 'local_min', 'local_max']
         all_columns = [i + '_' + j for i in function_columns for j in df_columns] + ['month']
-        all_SKU = sorted(list(set(self.df['New_SKU'])))
+        all_SKU = sorted(list(set(self.df['Format_SKU'])))
         self.new_df = pd.DataFrame(data=np.zeros((len(all_SKU), len(all_columns))), index=all_SKU, columns=all_columns)
 
-    def fill_new_data(self):
+    def fill_new_data(self):  # fill new pandas DataFrame by using our functions
         global table
         for i in range(0, 8, 2):
-            table = pd.pivot_table(self.df, values=self.df.columns.values[int(1 + i / 2)], index=['New_SKU'],
+            table = pd.pivot_table(self.df, values=self.df.columns.values[int(1 + i / 2)], index=['Format_SKU'],
                                    columns=['Месяц'], fill_value=0)
             self.new_df.loc[:, self.new_df.columns[i]] = table.apply(lambda x: find_max(x), axis=1)
             self.new_df.loc[:, self.new_df.columns[i + 1]] = table.apply(
@@ -149,4 +144,3 @@ class CreateNewData:
             self.new_df.loc[:, self.new_df.columns[i + 41]] = table.apply(
                 lambda x: find_min_quantites(x.values[:-1] - x.values[1:]), axis=1)
         self.new_df.loc[:, self.new_df.columns[48]] = table.apply(lambda x: find_quantity_month(x), axis=1)
-
