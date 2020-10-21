1398413ac597dfd28ebb7c01690f150558f443c8
unknown <leshhuk.2000@mail.ru>
2020-10-21 21:58:23 +0300

    change train

--- models/train.py
+++ models/train.py
@@ -40,8 +40,8 @@ class Trainer:
                            "reg_alpha": (0, 10), "reg_lambda": (0, 10),
                            "scale_pos_weight": (1, 25)
                            }
-        self.cat_pounds = {"learning_rate": (0.00001, 1), "depth": (3, 10),
-                           "l2_leaf_reg": (0, 30),
+        self.cat_pounds = {"n_estimators": (50, 350), "learning_rate": (0.00001, 1),
+                           "depth": (3, 10), "l2_leaf_reg": (0, 30),
                            "grow_policy": (0, 2), "min_data_in_leaf": (1, 20),
                            "bagging_temperature": (0, 10), "random_strength": (1, 20)
                            }
@@ -90,6 +90,10 @@ class Trainer:
         optimizer_lgbm.maximize(n_iter=500)
         optimizer_cat.maximize(n_iter=500)
 
+    def log_par_and_metrics(self, params, metric):
+        [mlflow.log_param(i, j) for i, j in zip(params.keys(), params.values())]
+        mlflow.log_metric("f1_score", metric)
+
     def lgbm(self, n_estimators, boosting_type, num_leaves, class_weight, learning_rate, subsample_for_bin,
              min_split_gain, min_child_weight, min_child_samples, subsample):
         estimator = LGBMClassifier(
@@ -128,9 +132,10 @@ class Trainer:
         )
         return estimator
 
-    def cat(self, learning_rate, depth, l2_leaf_reg, grow_policy, min_data_in_leaf, bagging_temperature,
+    def cat(self, n_estimators, learning_rate, depth, l2_leaf_reg, grow_policy, min_data_in_leaf, bagging_temperature,
             random_strength):
         estimator = CatBoostClassifier(
+            n_estimators=int(n_estimators),
             iterations=200,
             learning_rate=learning_rate,
             depth=int(depth),
@@ -145,69 +150,31 @@ class Trainer:
         )
         return estimator
 
-    def lgbm_cv(self, n_estimators, boosting_type, num_leaves, class_weight, learning_rate, subsample_for_bin,
-                min_split_gain, min_child_weight, min_child_samples, subsample):
-        estim = self.lgbm(n_estimators, boosting_type, num_leaves, class_weight, learning_rate, subsample_for_bin,
-                          min_split_gain, min_child_weight, min_child_samples, subsample)
+    def lgbm_cv(self, **kwargs):
+        estim = self.lgbm(**kwargs)
         cval = cross_val_score(estim, self.data, self.target,
                                scoring=make_scorer(f1_score), cv=5)
 
         with mlflow.start_run(run_name='lightgbm'):
-            mlflow.log_param('n_estimators', n_estimators)
-            mlflow.log_param('boosting_type', boosting_type)
-            mlflow.log_param('num_leaves', num_leaves)
-            mlflow.log_param('class_weight', class_weight)
-            mlflow.log_param('learning_rate', learning_rate)
-            mlflow.log_param('subsample_for_bin', subsample_for_bin)
-            mlflow.log_param('min_split_gain', min_split_gain)
-            mlflow.log_param('min_child_weight', min_child_weight)
-            mlflow.log_param('min_child_samples', min_child_samples)
-            mlflow.log_param('subsample', subsample)
-            mlflow.log_metric("f1_score", cval.mean())
+            self.log_par_and_metrics(estim.get_params(), cval.mean())
 
         return cval.mean()
 
-    def xgb_cv(self, max_depth, learning_rate, booster, gamma, min_child_weight, max_delta_step,
-               subsample, colsample_bytree, colsample_bylevel, colsample_bynode, reg_alpha, reg_lambda,
-               scale_pos_weight):
-        estim = self.xgb(max_depth, learning_rate, booster, gamma, min_child_weight, max_delta_step,
-                         subsample, colsample_bytree, colsample_bylevel, colsample_bynode,
-                         reg_alpha, reg_lambda, scale_pos_weight)
+    def xgb_cv(self, **kwargs):
+        estim = self.xgb(**kwargs)
         cval = cross_val_score(estim, self.data, self.target,
                                scoring=make_scorer(f1_score), cv=5)
         with mlflow.start_run(run_name='xgboost'):
-            mlflow.log_param('max_depth', max_depth)
-            mlflow.log_param('learning_rate', learning_rate)
-            mlflow.log_param('booster', booster)
-            mlflow.log_param('gamma', gamma)
-            mlflow.log_param('min_child_weight', min_child_weight)
-            mlflow.log_param('max_delta_step', max_delta_step)
-            mlflow.log_param('subsample', subsample)
-            mlflow.log_param('colsample_bytree', colsample_bytree)
-            mlflow.log_param('colsample_bylevel', colsample_bylevel)
-            mlflow.log_param('colsample_bynode', colsample_bynode)
-            mlflow.log_param('reg_alpha', reg_alpha)
-            mlflow.log_param('reg_lambda', reg_lambda)
-            mlflow.log_param('scale_pos_weight', scale_pos_weight)
-            mlflow.log_metric("f1_score", cval.mean())
+            self.log_par_and_metrics(estim.get_params(), cval.mean())
 
         return cval.mean()
 
-    def cat_cv(self, learning_rate, depth, l2_leaf_reg, grow_policy, min_data_in_leaf,
-               bagging_temperature, random_strength):
-        estim = self.cat(learning_rate, depth, l2_leaf_reg, grow_policy, min_data_in_leaf,
-                         bagging_temperature, random_strength)
+    def cat_cv(self, **kwargs):
+        estim = self.cat(**kwargs)
         cval = cross_val_score(estim, self.data, self.target,
                                scoring=make_scorer(f1_score), cv=5)
         with mlflow.start_run(run_name='catboost'):
-            mlflow.log_param('learning_rate', learning_rate)
-            mlflow.log_param('depth', depth)
-            mlflow.log_param('l2_leaf_reg', l2_leaf_reg)
-            mlflow.log_param('grow_policy', grow_policy)
-            mlflow.log_param('min_data_in_leaf', min_data_in_leaf)
-            mlflow.log_param('bagging_temperature', bagging_temperature)
-            mlflow.log_param('random_strength', random_strength)
-            mlflow.log_metric("f1_score", cval.mean())
+            self.log_par_and_metrics(estim.get_params(), cval.mean())
 
         return cval.mean()
 
